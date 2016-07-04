1b17dbaaa58094bf4c18e735043b3a96bf0c129b
vsstack <varun.fullstack@gmail.com>
2016-07-05 00:32:47 +0530

    Add to search and compile java source file in mvn project structure

--- spring-boot-oauth/src/main/java/com/service/JavaCompiler.java
+++ spring-boot-oauth/src/main/java/com/service/JavaCompiler.java
@@ -15,18 +15,28 @@ import java.util.List;
 @Component
 public class JavaCompiler {
 
+    private final String MVN = "mvn";
+    private final String JAVA = "java";
+    private final String XML = "xml";
+    private final String SRC_PATH = "/src/main/java/com/";
+
     /**
-     *
-     * @param path
+     * This method compile java source code
+     * @param command
      * @param output
      * @throws IOException
      * @throws InterruptedException
      */
-    private void compileSource(String path, List<String> output) throws IOException, InterruptedException {
+    private void compileSource(String command, List<String> output) throws IOException, InterruptedException {
 
         Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec("javac " + path);
-        InputStream stderr = proc.getErrorStream();
+        Process proc = rt.exec(command);
+        InputStream stderr;
+        if (command.startsWith(MVN)){
+            stderr = proc.getInputStream();
+        } else {
+            stderr = proc.getErrorStream();
+        }
         InputStreamReader isr = new InputStreamReader(stderr);
         BufferedReader br = new BufferedReader(isr);
         String line = null;
@@ -40,8 +50,9 @@ public class JavaCompiler {
         proc.waitFor();
     }
 
+
     /**
-     *
+     * This method gets java class file in given directory
      * @param directoryPath
      * @return
      * @throws IOException
@@ -61,7 +72,9 @@ public class JavaCompiler {
         String line = null;
         List<String> lineStr = new ArrayList<>();
         while ( (line = br.readLine()) != null) {
-            if(line.endsWith("java")) {
+            if(line.endsWith(JAVA)) {
+                lineStr.add(line);
+            } else if(line.endsWith(XML)){
                 lineStr.add(line);
             }
         }
@@ -71,7 +84,7 @@ public class JavaCompiler {
 
 
     /**
-     *
+     * main method for java compilation process
      * @param path
      * @return
      * @throws IOException
@@ -79,14 +92,32 @@ public class JavaCompiler {
      */
     public List<String> compile(String path) throws IOException, InterruptedException
     {
-        //get mvn file in directory, if found then skip below step.
-        List<String> line = getJavaFilesInDirectory(path);
-        //send mvn command along with path
+        List<String> lsOutput = getJavaFilesInDirectory(path);
         List<String> output = new ArrayList<>();
-        for (String l: line){
-            String p = path + l;
-            compileSource(p, output);
+
+        if(lsOutput.contains("pom.xml")){
+            StringBuilder command = new StringBuilder()
+                    .append("mvn ")
+                    .append("-f ")
+                    .append(path)
+                    .append(" test");
+            compileSource(command.toString(), output);
+        } else {
+            //get java source file
+            StringBuilder absPath = new StringBuilder()
+                    .append(path)
+                    .append(SRC_PATH);
+            lsOutput = getJavaFilesInDirectory(absPath.toString());
+
+            if(lsOutput.size()<1){
+                 throw new IOException("Directory does not contain executable");
+            }
+            for (String l: lsOutput){
+                StringBuilder command = new StringBuilder().append("javac ").append(absPath).append(l);
+                compileSource(command.toString(), output);
+            }
         }
+
         if(output.isEmpty()){
             output.add("Compilation successful");
         }
@@ -94,19 +125,19 @@ public class JavaCompiler {
     }
 
 
-//    public static void main( String[] args ) throws Exception
-//    {
-//
-//        JavaCompiler jc= new JavaCompiler();
-//        String path = "/Users/varunsingh/Desktop/dfolder/cfolder/";
-//        List<String> output = jc.compile(path);
-//
-//        System.out.println("Below is the output...");
-//        for(String o: output){
-//            System.out.println(output);
-//        }
-//
-//    }
+    public static void main( String[] args ) throws Exception
+    {
+
+        JavaCompiler jc= new JavaCompiler();
+        String path = "/tmp/javasample-error/";
+        List<String> output = jc.compile(path);
+
+        System.out.println("Below is the output...");
+        for(String o: output){
+            System.out.println(output);
+        }
+
+    }
 
 
 }
